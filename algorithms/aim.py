994e3be148acba6a6d4818c742f8d31914f9c9d7
Massil HIHAT <massilhihat@gmail.com>
2023-05-05 15:08:43 +0200

    fixing AIM

--- algorithms/aim.py
+++ algorithms/aim.py
@@ -12,7 +12,7 @@ class AIM_algorithm(Algorithm) :
         self.holding_costs = holding_costs
         self.penalty_costs = penalty_costs
 
-        self.D = np.sqrt(np.sum((y_max-y_min)**(y_max-y_min)))
+        self.D = np.sqrt(np.sum((y_max-y_min)*(y_max-y_min)))
         self.reset()
 
     def next_decision(self, t,state, subgradient, sales,demands) :
@@ -24,6 +24,7 @@ class AIM_algorithm(Algorithm) :
             learning_rate = self.gamma*self.D/(self.G*np.sqrt(t-1))
             self.target_decision = np.clip(self.target_decision-learning_rate*target_subgradient,self.y_min,self.y_max)
             self.implemented_decision = np.maximum(state,self.target_decision)
+            assert (state <= self.implemented_decision).all(), "Weird stuff happenning"
         return np.array(self.implemented_decision)
 
     def __str__(self) :
